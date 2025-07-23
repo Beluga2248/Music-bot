24ad3b8a5848ae97268a45f296655417b3c0f0c1
Beluga2248 <113758661+Beluga2248@users.noreply.github.com>
2025-07-24 07:15:12 +0800

    pause.js

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -9,13 +9,13 @@ module.exports = {
 
     async execute({ inter }) {
         const queue = useQueue(inter.guild);
-        if (!queue?.isPlaying()) return inter.editReply({ content: await Translate(`No music currently playing <${inter.member}>... try again ? <❌>`) });
+        if (!queue?.isPlaying()) return inter.editReply({ content: await Translate(`<a:Wrong:1017416697168269372:> No music currently playing <${inter.member}>... try again ?`) });
 
-        if (queue.node.isPaused()) return inter.editReply({ content: await Translate(`The track is currently paused, <${inter.member}>... try again ? <❌>`) });
+        if (queue.node.isPaused()) return inter.editReply({ content: await Translate(`<a:Wrong:1017416697168269372:> The track is currently paused, <${inter.member}>... try again ?`) });
 
         const success = queue.node.setPaused(true);
         const pauseEmbed = new EmbedBuilder()
-            .setAuthor({ name: success ? await Translate(`Current music <${queue.currentTrack.title}> paused <✅>`) : await Translate(`Something went wrong <${inter.member}>... try again ? <❌>`) })
+            .setAuthor({ name: success ? await Translate(`Current music <${queue.currentTrack.title}> paused <a:Yes:1011614293420150805:>`) : await Translate(`Something went wrong <${inter.member}>... try again ? <a:Wrong:1017416697168269372:>`) })
             .setColor('#2f3136')
 
         return inter.editReply({ embeds: [pauseEmbed] });
